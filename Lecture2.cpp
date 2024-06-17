a88ba0555dbf0b13e5501ba21db4c17a6fcac849
Ashish0705 <165580207+Ashish0705@users.noreply.github.com>
2024-06-17 20:39:30 +0530

    Add files via upload

---
+++ Lecture2.cpp
@@ -0,0 +1,16 @@
+// Lecture2- Writing first program-Hello World 
+// And We are now starting our coding parts.
+
+#include<iostream>       //This is a Header Part.        
+
+
+using namespace std;      // std makes sure that cout is a part of " Iostream"
+
+
+int main(){       //we have written a main "Function "
+
+    cout<<"Hello world";     //This will print out hello world on the user screen .
+
+    return 0;
+
+}
