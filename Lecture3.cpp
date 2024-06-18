358727f33da20b4ed61b8d4725868f5f4397eb58
Ashish0705 <165580207+Ashish0705@users.noreply.github.com>
2024-06-18 20:37:27 +0530

    Add files via upload

---
+++ Lecture3.cpp
@@ -0,0 +1,5 @@
+// Variables, Constants And DAta Types 
+//  In this lecture we covered about 
+//  1. Variable and constants in cpp And 
+// 2. How will the computer identify the kind of input we give and so more .
+ 
