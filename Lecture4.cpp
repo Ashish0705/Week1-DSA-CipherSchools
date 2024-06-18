e8ed65abeaa583ed2233af348ab725e846ff2745
Ashish0705 <165580207+Ashish0705@users.noreply.github.com>
2024-06-18 20:59:26 +0530

    Add files via upload

---
+++ Lecture4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+// Decleare 2 variables and then assign them values of 3,5 and then add them 
+
+int a=3;
+int b=5;
+
+
+cout<<a+b<<endl<<"a+b";   //or
+
+
+// cout<<a+b;
+// cout<<endl;   //for line break we use "endl".  
+// cout<<"a+b";
+
+
+
+
+}
