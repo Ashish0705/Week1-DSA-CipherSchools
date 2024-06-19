52a8d9bb0377f01358fae9c52dda5949ecf2ffb8
Ashish0705 <165580207+Ashish0705@users.noreply.github.com>
2024-06-19 22:03:43 +0530

    Add files via upload

---
+++ Lecture7.cpp
@@ -0,0 +1,26 @@
+// Arrays pt-1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int x; // variable "x" can store a single integer which means x can either store 1 or 2 or any single integer
+
+     // Arrays can be used to store multiple values in a single variable name
+
+    // int a[5]; // This means it is an array of name a which can store multiple integers , it can store 6 integer
+              // Note: the indexing of arrays starts from 0 and not 1
+
+
+int a,b,c,d,e;
+
+cout<<"Give 5 integers as input"<<endl;
+cin>>a>>b>>c>>d>>e;
+
+cout<<"The integers in reverse order are: "<<e<<d<<c<<b<<a;
+
+return 0;
+
+
+}
