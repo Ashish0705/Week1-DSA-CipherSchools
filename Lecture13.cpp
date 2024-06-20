c7cff2b81a4cf7682b7e5ba2a46d72e32acffadf
Ashish0705 <165580207+Ashish0705@users.noreply.github.com>
2024-06-20 22:43:17 +0530

    Add files via upload

---
+++ Lecture13.cpp
@@ -0,0 +1,15 @@
+// Functions pt-1
+//in this lecture there is not any coding part 
+// This is introduction and theory part of function .
+
+#include<iostream>
+using namespace std;
+
+int main() // Every program is a " main " function 
+{
+    cout<<"Hello world";
+
+    return 0;
+
+
+}
