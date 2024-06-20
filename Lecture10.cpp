056742deeffa4d194d5be3435a0e521f63159111
Ashish0705 <165580207+Ashish0705@users.noreply.github.com>
2024-06-20 21:22:46 +0530

    Add files via upload

---
+++ Lecture10.cpp
@@ -0,0 +1,32 @@
+//Loops pt-2
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+
+// int i, j,k;
+// i=0;
+// j=0;
+// k=0;
+
+
+// cout<<i<<" "<<j<<" "<<k<<endl;
+// j=i++;
+
+// cout<<i<<" "<<j<<" "<<k<<endl;
+
+// k=++i;
+// cout<<i<<" "<<j<<" "<<k<<endl;
+
+int i;
+
+for(i=1; i<101 ;i++)// called the update which happen after the interaction  
+
+{
+   cout<<i<<" ";
+
+}
+}
