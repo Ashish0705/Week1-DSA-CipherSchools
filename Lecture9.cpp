6b0c70395afc4c213f0375692fd7333f91fa11f5
Ashish0705 <165580207+Ashish0705@users.noreply.github.com>
2024-06-20 20:59:07 +0530

    Add files via upload

---
+++ Lecture9.cpp
@@ -0,0 +1,15 @@
+//Loops pt-1
+// In this video we know about intro part of loops 
+// There is no coding portion . In Next lecture there is a coding part.
+#include<iostream>
+using namespace std;
+
+int main(){
+int i;
+
+    for(i=0;   ;i++)
+    {
+        cout<<i<<" ";
+
+    }
+}
